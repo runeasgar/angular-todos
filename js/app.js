98cc138451a64102a7ae0e66a5eb6af2b36e6209
Joshua Taylor <joshua.taylor@acquia.com>
2015-08-23 15:13:35 -0500

    working on acquia cloud

--- js/app.js
+++ js/app.js
@@ -36,6 +36,14 @@
   
   app.controller('TodosController', ['$http', '$log', function ($http, $log){
   
+  	// CONFIGURATION
+		// Token from rest/session/token
+		// via https://www.drupal.org/node/2405657
+		this.csrfToken = 'Cj8I8k2Hdk3jRkYaaipbyCQjRXH6jPGp4pIgolsEmMg';
+		this.baseUrl = 'http://angulartodos58wxe8mlwp.devcloud.acquia-sites.com/';
+		this.todosJsonUrl = 'todos/json';
+		// CONFIGURATION END
+		 
   	// Init stuff
 		var todosCtrl = this;
 		this.todos = [ ];
@@ -43,28 +51,27 @@
 		this.todoToComplete = { }
 		this.todoToIncomplete = { }
 		this.tab = "incomplete";
-		this.csrfToken = 'bC0PWrHskBpiGbdvIEcmOTwwAucGBbKbxYOuQ2ZIHR4';
 		
 		// Grab todos from Drupal
-		$http.get('/to-dos/json').success(function(data){
-			todosCtrl.todos = data;
+		$http.get(this.baseUrl + this.todosJsonUrl, { headers: {
+			'X-CSRF-Token': this.csrfToken,
+		}})
+		.then(function(result){
+			todosCtrl.todos = result.data;
 		});
-				
 		// Create a todo from AngularJS
 		this.createTodo = function(){
 			this.todoToCreate.type = { target_id: "to_do" };
 			this.todoToCreate.field_complete = [ { value: "0" } ];
-			this.todoToCreate._links = { type: { href: "http://drupal8-headless.dd:8083/rest/type/node/to_do" } };
-			// Token from rest/session/token is bC0PWrHskBpiGbdvIEcmOTwwAucGBbKbxYOuQ2ZIHR4
-			// via https://www.drupal.org/node/2405657
-			$http.post('http://drupal8-headless.dd:8083/entity/node',this.todoToCreate, { headers: {
+			this.todoToCreate._links = { type: { href: this.baseUrl+"rest/type/node/to_do" } };
+			$http.post(this.baseUrl+'entity/node',this.todoToCreate, { headers: {
+			'Accept': 'application/json',
 			'Content-Type': 'application/hal+json',
 			'X-CSRF-Token': this.csrfToken,
 			}})
-			.then(function(response){
-				var explodedLocation = response.headers('Location').split('/');
+			.then(function(result){
+				var explodedLocation = result.headers('Location').split('/');
 				todosCtrl.todoToCreate.nid = [ { value: explodedLocation[explodedLocation.length-1] } ];
-				$log.log(todosCtrl.todoToCreate);
 				todosCtrl.todos.unshift(todosCtrl.todoToCreate);
 				todosCtrl.todoToCreate = { };
 			});
@@ -75,15 +82,13 @@
 			this.todoToComplete.nid = [ { value: nidToComplete } ];
 			this.todoToComplete.type = { target_id: "to_do" };
 			this.todoToComplete.field_complete = [ { value: "1" } ];
-			this.todoToComplete._links = { type: { href: "http://drupal8-headless.dd:8083/rest/type/node/to_do" } };
-			$http.patch('http://drupal8-headless.dd:8083/node/'+this.todoToComplete.nid[0].value,this.todoToComplete, 
-			{ headers: {
+			this.todoToComplete._links = { type: { href: this.baseUrl+"rest/type/node/to_do" } };
+			$http.patch(this.baseUrl+'node/'+this.todoToComplete.nid[0].value,this.todoToComplete, { headers: {
 			'Content-Type': 'application/hal+json',
 			'X-CSRF-Token': this.csrfToken,
 			}})
-			.then(function(response){
+			.then(function(result){
 				var keyToComplete = "";
-				// Remove from UI - doesn't work after a todo has been created without refresh
 				for (var key in todosCtrl.todos) {
 					if (todosCtrl.todos[key].nid[0].value === nidToComplete) {
 						todosCtrl.todos[key].field_complete[0].value = 1;
@@ -98,21 +103,21 @@
 			this.todoToIncomplete.nid = [ { value: nidToIncomplete } ];
 			this.todoToIncomplete.type = { target_id: "to_do" };
 			this.todoToIncomplete.field_complete = [ { value: "0" } ];
-			this.todoToIncomplete._links = { type: { href: "http://drupal8-headless.dd:8083/rest/type/node/to_do" } };
-			$http.patch('http://drupal8-headless.dd:8083/node/'+this.todoToIncomplete.nid[0].value,this.todoToIncomplete, 
-			{ headers: {
+			this.todoToIncomplete._links = { type: { href: this.baseUrl+"rest/type/node/to_do" } };
+			$http.patch(this.baseUrl+'node/'+this.todoToIncomplete.nid[0].value,this.todoToIncomplete, { headers: {
 			'Content-Type': 'application/hal+json',
-			'X-CSRF-Token': 'bC0PWrHskBpiGbdvIEcmOTwwAucGBbKbxYOuQ2ZIHR4'
+			'X-CSRF-Token': this.csrfToken,
 			}})
-			.then(function(response){
+			.then(function(result){
 				var keyToComplete = "";
-				// Remove from UI - doesn't work after a todo has been created without refresh
 				for (var key in todosCtrl.todos) {
 					if (todosCtrl.todos[key].nid[0].value === todosCtrl.todoToIncomplete.nid[0].value) {
 						todosCtrl.todos[key].field_complete[0].value = 0;
 					}
 				}
 				todosCtrl.todoToIncomplete = { };
+			},function(result){
+				$log.log(result);
 			});
 		};
 		
